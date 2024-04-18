309c7796ddc0f8b77c49164e79f10ca0e71fb2e5
saeid shabani <django_saeid.sh@proton.me>
2024-04-19 00:57:05 +0330

    create custom user

--- config/settings.py
+++ config/settings.py
@@ -37,6 +37,11 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    # third party apps
+
+
+    # we install these apps
+    'accounts',
 ]
 
 MIDDLEWARE = [
@@ -127,3 +132,5 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+# custom user
+AUTH_USER_MODEL = 'accounts.CustomUser'
